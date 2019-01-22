69af2ca71552dede391f926012e26daea6dd334f
cris93g <37388115+cris93g@users.noreply.github.com>
2019-01-22 15:26:04 -0600

    started to get data from api

---
+++ server/WeatherController/WeatherController.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const axios = require("axios");
+
+let mesquite = process.env.apiKey;
+
+axios
+	.get(
+		`http://dataservice.accuweather.com/forecasts/v1/daily/1day/339375?${mesquite}`
+	)
+	.then(response => {
+		weather = response.data;
+		console.log(weather);
+		return weather;
+	});
+
+let getWeather = (req, res) => {
+	res.status(200).send(weather);
+	console.log(weather);
+};
+//exporting the functions
+module.exports = {
+	getWeather
+};
--- server/index.js
+++ server/index.js
@@ -4,8 +4,13 @@ const { json } = require("body-parser");
 const massive = require("massive");
 const cors = require("cors");
 const app = express();
-
 const port = process.env.port || 3001;
+const weatherCtrl = require("./WeatherController/WeatherController");
+
+app.use(cors());
+app.use(json());
+
+app.get("/api/weather", weatherCtrl.getWeather);
 
 app.listen(port, () => {
 	console.log(`weather app listening on port ${port}`);
